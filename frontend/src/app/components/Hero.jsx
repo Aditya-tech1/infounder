c30efdabadb58da31007ee664e6b1a02d7b0d27f
aka7898 <132600421+aka7898@users.noreply.github.com>
2025-06-28 20:11:52 +0530

    component

---
+++ frontend/src/app/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GridLines } from '../../public/icons/icon'
+
+export default function Hero() {
+  return (
+    <div className='flex flex-row w-full h-[90vh] items-center justify-center gap-24'>
+
+      <span className='gradient_cloud '></span>
+      <GridLines className='absolute top-1/7 right-1/7 w-1/3 aspect-square z-5' />
+
+
+      <div className='flex flex-col w-1/2 h-full justify-center pl-24 gap-8'>
+        <h1 className='text-5xl text-[#DDDDDD] font-serif font-bold w-3/4 leading-16 drop-shadow-sm drop-shadow-[#FF00BF]'>
+          Level up your presentation with AI feedback
+        </h1>
+        <p className='text-2xl w-full italic font-extralight tracking-wider'>
+          Get real-time insights on your tone, <br/> body language, clarity, and slides.
+        </p>
+        <div style={{background: "linear-gradient(90deg, rgba(220, 0, 211, 0.8) 0%, rgba(12, 250, 245, 0.8) 100%)"}} className=' flex flex-row w-fit rounded-full items-center justify-center'>
+
+          <Link href="/upload" className='font-bold text-2xl p-4 px-10  border-white w-fit rounded-full   drop-shadow-black drop-shadow-sm'>Try Free Demo</Link>
+        </div>
+      </div>
+
+      <div className='h-full w-1/2'>
+        <Image src="/pink_man_clear.png" alt='pink man image' width={1920} height={1920} className='h-full  w-auto  relative -right-16 z-10' />
+      </div>
+    </div>
+  )
+}
